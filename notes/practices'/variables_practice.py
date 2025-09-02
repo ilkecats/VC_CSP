2a7d5bd46647c3df72a10994151aeb698cc29ed9
ilikecats <angel.camacho@usas-edu.net>
2025-09-02 13:44:59 -0600

    messsaje

---
+++ notes/practices'/variables_practice.py
@@ -0,0 +1,22 @@
+#AC 6th Variables practice
+name= "angel" 
+number="8"
+num="678"
+breakfeast="arepas"
+color="green"
+school="ucas"
+year="2025"
+eye_color="brown"
+age="14"
+subject = "advisory"
+
+print(name)
+print(number)
+print(num)
+print(breakfeast)
+print(color)
+print(school)
+print(year)
+print(eye_color)
+print(age)
+print(subject)
