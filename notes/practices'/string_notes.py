573939899acf47150eeb6de17d609b7cb079d99e
ilikecats <angel.camacho@usas-edu.net>
2025-09-08 13:45:30 -0600

    string notes

--- notes/practices'/string_notes.py
+++ notes/practices'/string_notes.py
@@ -1,3 +1,13 @@
 # AC 6th string notes 
 
 print("I did it!") 
+
+first_name=input("whats is your name:\n").strip().upper().capitalize()
+
+last_name=input("what is your lastname")
+
+full_name=first_name=last_name
+
+sentence= "quick brown fox jumps over the lazy dog."
+
+print("welcome to my program",name, "!") 
