d3610a45b27bd70289aa3c14e53f09f255d1ebdf
ilikecats <angel.camacho@usas-edu.net>
2025-09-10 13:42:59 -0600

    silly_sentences

---
+++ notes/practices'/silly_sentences.py
@@ -0,0 +1,9 @@
+# AC 6TH silly_sententences
+
+thing=input("give me a thing")
+place=input("give me a place")
+color=input("give me a color")
+verb=input("give me a verb")
+name=input("give me a name")
+
+print("she have a ", thing, "and before going to ", )
